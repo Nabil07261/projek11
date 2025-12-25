85ecb13c74f958498a71a05f876d3192a4b076b5
nabotz <labqino29@gmail.com>
2025-12-25 14:49:06 +0700

    kedua

--- tampilDataMhs.php
+++ tampilDataMhs.php
@@ -6,57 +6,51 @@
 <html lang="en">
 <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Data Mahasiswa</title>
-        <link rel="stylesheet" href="style.css">
 </head>
 <body>
-        <div class="container-large">
-                <div class="header-section">
-                        <h2>Daftar Data Mahasiswa</h2>
-                        <a href="tambahDataMhs.php" class="btn-tambah">+ Tambah Data Baru</a>
-                </div>
 
-                <table>
-                        <thead>
-                                <tr>
-                                        <th>ID</th>
-                                        <th>NIM</th>
-                                        <th>Nama</th>
-                                        <th>Tempat Lahir</th>
-                                        <th>Tanggal Lahir</th>
-                                        <th>Jml Sdr</th>
-                                        <th>Alamat</th>
-                                        <th>Kota</th>
-                                        <th>JK</th>
-                                        <th>Status</th>
-                                        <th>Hobi</th>
-                                        <th>Email</th>
-                                        <th>Pass</th>
-                                </tr>
-                        </thead>
-                        <tbody>
-                                <?php while ($row = mysqli_fetch_assoc($data)) : ?>
-                                <tr>
-                                        <td><?= $row['id'] ?></td>
-                                        <td><?= $row['nim'] ?></td>
-                                        <td><?= $row['nama'] ?></td>
-                                        <td><?= $row['tempatLahir'] ?></td>
-                                        <td><?= $row['tanggalLahir'] ?></td>
-                                        <td><?= $row['jmlSaudara'] ?></td>
-                                        <td><?= $row['alamat'] ?></td>
-                                        <td><?= $row['kota'] ?></td>
-                                        <td><?= $row['jenisKelamin'] ?></td>
-                                        <td><?= $row['statusKeluarga'] ?></td>
-                                        <td><?= $row['hobi'] ?></td>
-                                        <td><?= $row['email'] ?></td>
-                                        <td><?= substr($row['pass'], 0, 20) ?>...</td>
-                                </tr>
-                                <?php
-                                endwhile;
-                                ?>
-                        </tbody>
-                </table>
-        </div>
+        <h2>Daftar Data Mahasiswa</h2>
+        <a href="tambahDataMhs.php">Tambah Data Baru</a>
+        <br><br>
+
+        <table border="1" cellpadding="8">
+                <tr>
+                        <th>ID</th>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>Tempat Lahir</th>
+                        <th>Tanggal Lahir</th>
+                        <th>Jml Sdr</th>
+                        <th>Alamat</th>
+                        <th>Kota</th>
+                        <th>JK</th>
+                        <th>Status</th>
+                        <th>Hobi</th>
+                        <th>Email</th>
+                        <th>Pass</th>
+                </tr>
+
+                <?php while ($row = mysqli_fetch_assoc($data)) : ?>
+                <tr>
+                        <td><?= $row['id'] ?></td>
+                        <td><?= $row['nim'] ?></td>
+                        <td><?= $row['nama'] ?></td>
+                        <td><?= $row['tempatLahir'] ?></td>
+                        <td><?= $row['tanggalLahir'] ?></td>
+                        <td><?= $row['jmlSaudara'] ?></td>
+                        <td><?= $row['alamat'] ?></td>
+                        <td><?= $row['kota'] ?></td>
+                        <td><?= $row['jenisKelamin'] ?></td>
+                        <td><?= $row['statusKeluarga'] ?></td>
+                        <td><?= $row['hobi'] ?></td>
+                        <td><?= $row['email'] ?></td>
+                        <td><a href="koreksiDataMhs.php?kode=<?php echo $row['id']?>">Koreksi</a></td>
+                        <td><a href="hapusDataMhs.php?kode=<?php echo $row["id"]?>"onclick="return confirm('Yakin dihapus?')">Hapus</a></td>
+                </tr>
+                <?php
+                endwhile;
+                ?>
+        </table>
 </body>
 </html>
